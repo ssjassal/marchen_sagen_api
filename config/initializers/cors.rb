98df7d4ce333f261df2cd66e553855a9959a010f
Soniya Jassal <jassal_soniya@bah.com>
2017-06-02 11:35:11 -0400

    Heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://still-fortress-frontend-94098.herokuapp.com'
 
     resource '*',
       headers: :any,
